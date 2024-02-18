83198ac8803595ecd47fba899a6b1a59eb2d8fc0
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-18 10:29:22 -0400

    Bones to project started

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initialize our data to start
+        Structures dataStructure = new Structures();
     }
 }
