b304f0ca8f800f0c056d7401990015768020e965
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-18 10:29:04 -0400

    Added skeleton methods

--- src/Structures.java
+++ src/Structures.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Structures {
+public class Structures {
+
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        // Node constructor
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    // Initialize to null globally
+    Node head, tail = null;
+
+    // Append adds a new node containing a value to the end of the list
+    private void append() {
+
+    }
+
+    // Adds a new node to the start of the list
+    private void prepend() {
+
+    }
+
+    // Returns the total number of nodes in the list
+    private void nodeSize() {
+
+    }
+
+    // Returns the first node in the list
+    private void nodeHead() {
+
+    }
+
+    // Returns the last node in the list
+    private void nodeTail() {
+
+    }
+
+    // Returns the node at the given index
+    private void atPosition(int index) {
+
+    }
+
+    // Removes last element from the list
+    private void nodePop() {
+
+    }
+
+    // Returns true if the passed value is in the list, else, return false
+    private void nodeContains(int value) {
+
+    }
+
+    // Returns the index of the node containing the value, null if not found
+    private void nodeFind(int value) {
+
+    }
 }
