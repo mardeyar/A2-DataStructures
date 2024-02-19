08b3fca3a88c9335b72a6efe972ac2bdc8a1feb9
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-19 14:21:15 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,67 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // Initialize our data to start
         Structures dataStructure = new Structures();
+        Random random = new Random();
+
+        // Append method testing
+        for (int i = 0; i < 9; i++) {
+            dataStructure.append(random.nextInt(20));
+        }
+
+        System.out.println("Data structure after append method:");
+        for (int i = 0; i < dataStructure.nodeSize(); i++) {
+            System.out.print(dataStructure.atPosition(i).data + " ");
+        }
+
+        // Prepend method testing
+        dataStructure.prepend(random.nextInt(20));
+
+        System.out.println("\n\nData structure after prepend method:");
+        for (int i = 0; i < dataStructure.nodeSize(); i++) {
+            System.out.print(dataStructure.atPosition(i).data + " ");
+        }
+
+        // Size method testing
+        System.out.println("\n\nSize of list: " + dataStructure.nodeSize() + " values");
+
+        // Head method testing
+        System.out.println("\nHead value: " + dataStructure.nodeHead().data);
+
+        // Tail method testing
+        System.out.println("\nTail value: " + dataStructure.nodeTail().data);
+
+        // At position method testing
+        int nodeIndex = random.nextInt(dataStructure.nodeSize());
+        System.out.println("\nValue at index " + nodeIndex + ": " + dataStructure.atPosition(nodeIndex).data);
+
+        // Pop method testing
+        System.out.println("\nData structure before pop method:");
+        for (int i = 0; i < dataStructure.nodeSize(); i++) {
+            System.out.print(dataStructure.atPosition(i).data + " ");
+        }
+
+        System.out.println("\nRemoving the last element which is " + dataStructure.nodeTail().data);
+        dataStructure.nodePop();
+
+        System.out.println("\nData structure after pop method:");
+        for (int i = 0; i < dataStructure.nodeSize(); i++) {
+            System.out.print(dataStructure.atPosition(i).data + " ");
+        }
+
+        // Contains method testing
+        int containsValue = random.nextInt(20);
+        System.out.println("\n\nDoes the list contain " + containsValue + ": " + dataStructure.nodeContains(containsValue));
+
+        // Find method testing
+        int value = random.nextInt(20);
+        Integer index = dataStructure.nodeFind(value);
+        if (index != null) {
+            System.out.println("\nFound " + value + " at index " + index);
+        } else {
+            System.out.println("\nValue " + value + " not found");
+        }
     }
 }
--- src/Structures.java
+++ src/Structures.java
@@ -15,47 +15,103 @@ public class Structures {
     Node head, tail = null;
 
     // Append adds a new node containing a value to the end of the list
-    private void append() {
-
+    public void append(int value) {
+        if (!nodeContains(value)) {
+            Node newNode = new Node(value);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }
+        }
     }
 
     // Adds a new node to the start of the list
-    private void prepend() {
-
+    public void prepend(int value) {
+        if (!nodeContains(value)) {
+            Node newNode = new Node(value);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+        }
     }
 
     // Returns the total number of nodes in the list
-    private void nodeSize() {
-
+    public int nodeSize() {
+        int nodeCount = 0;
+        Node current = head;
+        while (current != null) {
+            nodeCount++;
+            current = current.next;
+        }
+        return nodeCount;
     }
 
     // Returns the first node in the list
-    private void nodeHead() {
-
+    public Node nodeHead() {
+        return head;
     }
 
     // Returns the last node in the list
-    private void nodeTail() {
-
+    public Node nodeTail() {
+        return tail;
     }
 
     // Returns the node at the given index
-    private void atPosition(int index) {
-
+    public Node atPosition(int index) {
+        if (index < 0 || index >= nodeSize()) {
+            return null;
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
     }
 
     // Removes last element from the list
-    private void nodePop() {
-
+    public void nodePop() {
+        if (tail != null) {
+            tail = tail.prev;
+            if (tail != null) {
+                tail.next = null;
+            } else {
+                head = null;
+            }
+        }
     }
 
     // Returns true if the passed value is in the list, else, return false
-    private void nodeContains(int value) {
-
+    public boolean nodeContains(int value) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == value) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
     }
 
     // Returns the index of the node containing the value, null if not found
-    private void nodeFind(int value) {
-
+    public Integer nodeFind(int value) {
+        int index = 0;
+        Node current = head;
+        while (current != null) {
+            if (current.data == value) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return null;
     }
 }
