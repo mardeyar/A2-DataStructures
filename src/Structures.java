347ecf3df663a921fad0972d7c0d4787660f8c7f
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-23 15:30:46 -0400

    Documented method parameters and return values

--- src/Structures.java
+++ src/Structures.java
@@ -1,6 +1,9 @@
 public class Structures {
 
-    class Node {
+    /**
+     * Class to represent a node in a doubly linked list
+     */
+    static class Node {
         int data;
         Node prev;
         Node next;
@@ -14,22 +17,27 @@ public class Structures {
     // Initialize to null globally
     Node head, tail = null;
 
-    // Append adds a new node containing a value to the end of the list
+    /**
+     * Appends a new node containing a value to the end of the list if the value is not already present
+     * @param value Value to be appended
+     */
     public void append(int value) {
         if (!nodeContains(value)) {
             Node newNode = new Node(value);
             if (head == null) {
                 head = newNode;
-                tail = newNode;
             } else {
                 tail.next = newNode;
                 newNode.prev = tail;
-                tail = newNode;
             }
+            tail = newNode;
         }
     }
 
-    // Adds a new node to the start of the list
+    /**
+     * Adds a new node containing the specified value to the start of the list
+     * @param value Value to be prepended
+     */
     public void prepend(int value) {
         if (!nodeContains(value)) {
             Node newNode = new Node(value);
@@ -44,7 +52,10 @@ public class Structures {
         }
     }
 
-    // Returns the total number of nodes in the list
+    /**
+     * Finds the total number of nodes in the list
+     * @return Total number of nodes
+     */
     public int nodeSize() {
         int nodeCount = 0;
         Node current = head;
@@ -55,17 +66,27 @@ public class Structures {
         return nodeCount;
     }
 
-    // Returns the first node in the list
+    /**
+     * Get the first node in the list
+     * @return First node in list
+     */
     public Node nodeHead() {
         return head;
     }
 
-    // Returns the last node in the list
+    /**
+     * Get the last node in the list
+     * @return Last node in list
+     */
     public Node nodeTail() {
         return tail;
     }
 
-    // Returns the node at the given index
+    /**
+     * Gets the node at the given index
+     * @param index Index of the node to be returned
+     * @return The node at the specified index, or, null if index is out of range
+     */
     public Node atPosition(int index) {
         if (index < 0 || index >= nodeSize()) {
             return null;
@@ -77,7 +98,9 @@ public class Structures {
         return current;
     }
 
-    // Removes last element from the list
+    /**
+     * Removes last element from the list
+     */
     public void nodePop() {
         if (tail != null) {
             tail = tail.prev;
@@ -89,7 +112,11 @@ public class Structures {
         }
     }
 
-    // Returns true if the passed value is in the list, else, return false
+    /**
+     * Takes a value to check whether the list contains a node with the given value
+     * @param value Value to be searched for
+     * @return True: if the passed value is in the list. False: if passed value is NOT in the list
+     */
     public boolean nodeContains(int value) {
         Node current = head;
         while (current != null) {
@@ -101,7 +128,11 @@ public class Structures {
         return false;
     }
 
-    // Returns the index of the node containing the value, null if not found
+    /**
+     * Searches the list for a node containing the passed value
+     * @param value Value to be searched
+     * @return Index of the node containing the value, or, null if not found
+     */
     public Integer nodeFind(int value) {
         int index = 0;
         Node current = head;
