cb4eb09a55115cb149a250d7d5d022826b4741cf
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-23 15:42:22 -0400

    Formatted some output

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,48 @@ public class Main {
         Structures dataStructure = new Structures();
         Random random = new Random();
 
+        System.out.println("\n****** DATA STRUCTURE METHOD TESTING ******\n");
+
         // Append method testing
+        System.out.println("=== Append method ===");
         for (int i = 0; i < 9; i++) {
             dataStructure.append(random.nextInt(20));
         }
 
-        System.out.println("Data structure after append method:");
+        System.out.print("Data structure after append method: ");
         for (int i = 0; i < dataStructure.nodeSize(); i++) {
             System.out.print(dataStructure.atPosition(i).data + " ");
         }
 
         // Prepend method testing
+        System.out.println("\n\n=== Prepend method ===");
         dataStructure.prepend(random.nextInt(20));
 
-        System.out.println("\n\nData structure after prepend method:");
+        System.out.print("Data structure after prepend method: ");
         for (int i = 0; i < dataStructure.nodeSize(); i++) {
             System.out.print(dataStructure.atPosition(i).data + " ");
         }
 
         // Size method testing
-        System.out.println("\n\nSize of list: " + dataStructure.nodeSize() + " values");
+        System.out.println("\n\n=== Node size method ===");
+        System.out.println("Size of list: " + dataStructure.nodeSize() + " values");
 
         // Head method testing
-        System.out.println("\nHead value: " + dataStructure.nodeHead().data);
+        System.out.println("\n=== Node head method ===");
+        System.out.println("Head value: " + dataStructure.nodeHead().data);
 
         // Tail method testing
-        System.out.println("\nTail value: " + dataStructure.nodeTail().data);
+        System.out.println("\n=== Node tail method ===");
+        System.out.println("Tail value: " + dataStructure.nodeTail().data);
 
         // At position method testing
+        System.out.println("\n=== Node position method ===");
         int nodeIndex = random.nextInt(dataStructure.nodeSize());
-        System.out.println("\nValue at index " + nodeIndex + ": " + dataStructure.atPosition(nodeIndex).data);
+        System.out.println("Value at index " + nodeIndex + ": " + dataStructure.atPosition(nodeIndex).data);
 
         // Pop method testing
-        System.out.println("\nData structure before pop method:");
+        System.out.println("\n=== Pop method ===");
+        System.out.print("Data structure before pop method: ");
         for (int i = 0; i < dataStructure.nodeSize(); i++) {
             System.out.print(dataStructure.atPosition(i).data + " ");
         }
@@ -46,22 +55,24 @@ public class Main {
         System.out.println("\nRemoving the last element which is " + dataStructure.nodeTail().data);
         dataStructure.nodePop();
 
-        System.out.println("\nData structure after pop method:");
+        System.out.print("Data structure after pop method: ");
         for (int i = 0; i < dataStructure.nodeSize(); i++) {
             System.out.print(dataStructure.atPosition(i).data + " ");
         }
 
         // Contains method testing
+        System.out.println("\n\n=== Node contains method ===");
         int containsValue = random.nextInt(20);
-        System.out.println("\n\nDoes the list contain " + containsValue + ": " + dataStructure.nodeContains(containsValue));
+        System.out.println("Does the list contain " + containsValue + ": " + dataStructure.nodeContains(containsValue));
 
         // Find method testing
+        System.out.println("\n=== Node find method ===");
         int value = random.nextInt(20);
         Integer index = dataStructure.nodeFind(value);
         if (index != null) {
-            System.out.println("\nFound " + value + " at index " + index);
+            System.out.println("Found '" + value + "' at index " + index);
         } else {
-            System.out.println("\nValue " + value + " not found");
+            System.out.println("Value '" + value + "': not found");
         }
     }
 }
